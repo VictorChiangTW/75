ceff0b4e0095fba15ebebf67ffe1f49d3bdbddea
VictorChiangTW <45275788+VictorChiangTW@users.noreply.github.com>
2022-11-01 09:23:09 +0000

    refactor

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 #要先讀檔
-txt = []
-with open('input.txt', 'r') as f:
-  for line in f:
-    txt.append(line.strip())
+def readfile(file_name):
+  txt = []
+  with open(file_name, 'r') as f:
+    for line in f:
+      txt.append(line.strip())
+  return txt
 
-print(txt)
 
 #轉換檔案
-new = []
-for line in txt:
-  if line == 'Allen':
-    person = 'Allen'
-    continue
-  elif line == 'Tom':
-    person = 'Tom'
-    continue
-  new.append(person + ': ' + line)
+def convert(txt):
+  new = []
+  for line in txt:
+    if line == 'Allen':
+      person = 'Allen'
+      continue
+    elif line == 'Tom':
+      person = 'Tom'
+      continue
+    new.append(person + ': ' + line)
+  return (new)
 
-print(new)
 
 #寫出檔案
-with open('output.txt', 'w') as f:
-  for line in new:
-    f.write(line + '\n')
+def write_outfile(file_name, new):
+  with open(file_name, 'w') as f:
+    for line in new:
+      f.write(line + '\n')
+
+
+def main(file_name_1, file_name_2):
+  txt = readfile(file_name_1)
+  new = convert(txt)
+  write_outfile(file_name_2, new)
+
+
+main('input.txt', 'output.txt')
